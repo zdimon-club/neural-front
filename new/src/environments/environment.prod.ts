04ac13e66427146221eabe1df3c53e0594b6e9ad
Dmitry Zharikov <zdimon77@gmail.com>
2020-04-05 11:34:43 +0300

    commit from zdimon77@gmail.com ----

--- new/src/environments/environment.prod.ts
+++ new/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://neuro.webmonstr.com',
-  socketUrl: 'http://neuro.webmonstr.com',
+  apiUrl: 'http://api.n.webmonstr.com',
+  socketUrl: 'http://api.n.webmonstr.com',
   OPENVIDU_SERVER_URL: 'https://video.neuraldating.com:4443',
   OPENVIDU_SERVER_SECRET: 'SECRET',
   GOOGLE_AUTH: '769722970237-8lhn2anpmkjjagu1monnvooetln72i2b.apps.googleusercontent.com',
--- new/src/environments/environment.remote.ts
+++ new/src/environments/environment.remote.ts
@@ -2,8 +2,8 @@
 export const environment = {
   production: false,
   hmr: false,
-  apiUrl: 'https://ng-dating-test.webmonstr.com',
-  socketUrl: 'https://ng-dating-test.webmonstr.com',
+  apiUrl: 'https://api.n.webmonstr.com',
+  socketUrl: 'https://api.n.webmonstr.com',
   OPENVIDU_SERVER_URL: 'https://video.neuraldating.com:4443',
   OPENVIDU_SERVER_SECRET: 'SECRET',
   GOOGLE_AUTH: '769722970237-8lhn2anpmkjjagu1monnvooetln72i2b.apps.googleusercontent.com'
