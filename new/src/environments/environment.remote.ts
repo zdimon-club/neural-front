23968b28eaa2661b745c55d138f76cb9abc6e509
Dmitry Zharikov <zdimon77@gmail.com>
2020-04-05 12:05:48 +0300

    commit from zdimon77@gmail.com --https--

--- new/src/environments/environment.remote.ts
+++ new/src/environments/environment.remote.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: false,
   hmr: false,
   apiUrl: 'https://api.n.webmonstr.com',
-  socketUrl: 'https://api.n.webmonstr.com',
+  socketUrl: 'wss://api.n.webmonstr.com',
   OPENVIDU_SERVER_URL: 'https://video.neuraldating.com:4443',
   OPENVIDU_SERVER_SECRET: 'SECRET',
   GOOGLE_AUTH: '769722970237-8lhn2anpmkjjagu1monnvooetln72i2b.apps.googleusercontent.com'
